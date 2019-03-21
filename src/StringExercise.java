89848d9f412827936eb95130d3a44cd820bea837
Erik Behnke <erik.behnke@gmail.com>
2019-03-21 11:12:44 -0500

    complete java string exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,33 +1,32 @@
 public class StringExercise {
     public static void main(String[] args) {
-
-
-
-
+//        int a = 0;
+//        char b = 'b';
+//
+//        System.out.println(a + b);
+//        System.out.println(b + 0);
+//        System.out.println(b == 98);
+//        System.out.println(b);
+//
+//        System.out.println('b' + 5);
 
 
         //For each of the following output examples, create a string variable named message that contains the desired output and print it out to the console.
-        //
         //Do this with only one string variable and one print statement.
         //
         //
         //We don't need no education
+        System.out.println("We don't need no education");
         //We don't need no thought control
-        //
+        System.out.println("We don't need no thought control");
         //Check "this" out!, "s inside of "s!
-        //
+        System.out.println("Check this out!, \"s inside of \"s! ");
         //In windows, the main drive is usually C:\
-        //
+        System.out.println("In windows, the main drive is usually C:\\");
         //I can do backslashes \, double backslashes \\,
+        System.out.println("I can do backslashes \\, double backslashes \\\\,");
         //and the amazing triple backslash \\\!
-        //Create a class named Bob with a main method for the following exercise.
-        //
-        //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-        //
-        //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-        //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-        //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-        //He answers 'Whatever.' to anything else.
-        //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+        System.out.println("and the amazing triple backslash \\\\\\!");
+
     }
 }
--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -1,15 +1,14 @@
 public class fizzBuzz {
     public static void main(String[] args) {
-//        int i = 0;
-        for(int i = 0; i < 100; i+=1) {
-            if (i % 15 == 0) {
+        for(int i = 1; i < 60; i++) {
+            if (i % 15 == 0)
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            else if (i % 3 == 0)
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            else if (i % 5 == 0)
                 System.out.println("Buzz");
-            }
-            System.out.println(i);
+            else
+                System.out.println(i);
         }
     }
 }
